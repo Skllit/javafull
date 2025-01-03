601d474b2d7340782b00b57b0a3547582da36c7b
Likith Kumar S <sklallucination@gmail.com>
2025-01-03 15:02:58 +0530

    jan 3rd  ends at 4:0:0

--- hello.java
+++ hello.java
@@ -20,3 +20,24 @@
 
  //jan 2 (operators,)
     
+
+
+ // jan3 
+
+class clac{
+
+    int a; //instance variables  , what class knows
+    public int add(int a,int b){    // what class does,  behaviour
+        return a+b;
+    }
+}
+public class hello {
+
+    public static void main(String[] args) {
+
+        clac c= new clac(); // creaating object  named c  of type called as clac
+        int r = c.add(2,3);
+        System.out.println(r);
+
+    }
+}
